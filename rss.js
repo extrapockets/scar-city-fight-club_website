ee1dfba037bb45d8c3f21c21d9814ceb7c1830ba
extrapockets <extrapockets@proton.me>
2023-05-05 14:10:05 -0500

    adding rss.js to attempt sorting of three rss feeds

---
+++ rss.js
@@ -0,0 +1,53 @@
+const Parser = require("rss-parser");
+const moment = require("moment");
+
+const rssFeeds = [
+  {
+    name: "Bitcoin News",
+    url: "https://nobsbitcoin.com/rss/",
+  },
+  {
+    name: "Intel Techniques Blog",
+    url: "https://inteltechniques.com/blog/feed/",
+  },
+  {
+    name: "Buzzsprout Podcast",
+    url: "https://feeds.buzzsprout.com/1790481.rss",
+  },
+];
+
+async function getSortedHtml() {
+    try {
+      const parser = new Parser();
+      const promises = rssFeeds.map(async (feed) => {
+        const feedData = await parser.parseURL(feed.url);
+        const items = feedData.items.slice(0, 5); // limit to 5 items
+        const itemsWithDate = items.map((item) => {
+          return {
+            ...item,
+            date: moment.utc(item.pubDate).toDate(),
+          };
+        });
+        return itemsWithDate;
+      });
+      const itemsArrays = await Promise.all(promises);
+      const sortedItems = itemsArrays
+        .flat()
+        .sort((a, b) => moment.utc(b.date).diff(moment.utc(a.date)));
+      const itemsHtml = sortedItems.map((item) => {
+        return `<h3>${item.title}</h3><p>${item.contentSnippet}</p><p>${moment
+          .utc(item.date)
+          .format("MMMM Do YYYY, h:mm:ss a")}</p><a href="${item.link}">Read More</a>`;
+      });
+      const html = `<html><head><title>RSS Feed</title></head><body>${itemsHtml.join(
+        ""
+      )}</body></html>`;
+      return html;
+    } catch (error) {
+      console.error(error);
+      throw new Error("Error getting sorted RSS feed HTML");
+    }
+  }
+  
+module.exports = { getSortedHtml };
+
--- server.js
+++ server.js
@@ -1,39 +1,15 @@
 const cors = require("cors");
 const express = require("express");
-const Parser = require("rss-parser");
+const { getSortedHtml } = require("./rss"); // Import the rss.js module
 
 const app = express();
 const port = 3001;
 
 app.use(cors());
 
-const rssFeeds = [
-  {
-    name: "Bitcoin News",
-    url: "https://nobsbitcoin.com/rss/",
-  },
-  {
-    name: "Intel Techniques Blog",
-    url: "https://inteltechniques.com/blog/feed/",
-  },
-  {
-    name: "Buzzsprout Podcast",
-    url: "https://feeds.buzzsprout.com/1790481.rss",
-  },
-];
-
 app.get("/rss", async (req, res) => {
   try {
-    const parser = new Parser();
-    const promises = rssFeeds.map(async (feed) => {
-      const feedData = await parser.parseURL(feed.url);
-      const itemsHtml = feedData.items.map((item) => {
-        return `<h3>${item.title}</h3><p>${item.contentSnippet}</p><a href="${item.link}">Read More</a>`;
-      });
-      const html = `<html><head><title>${feed.name}</title></head><body>${itemsHtml.join("")}</body></html>`;
-      return html;
-    });
-    const html = (await Promise.all(promises)).join("");
+    const html = await getSortedHtml(); // Use the getSortedHtml() function
     res.send(html);
   } catch (error) {
     console.error(error);
@@ -44,3 +20,51 @@ app.get("/rss", async (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
+
+// const cors = require("cors");
+// const express = require("express");
+// const Parser = require("rss-parser");
+
+// const app = express();
+// const port = 3001;
+
+// app.use(cors());
+
+// const rssFeeds = [
+//   {
+//     name: "Bitcoin News",
+//     url: "https://nobsbitcoin.com/rss/",
+//   },
+//   {
+//     name: "Intel Techniques Blog",
+//     url: "https://inteltechniques.com/blog/feed/",
+//   },
+//   {
+//     name: "Buzzsprout Podcast",
+//     url: "https://feeds.buzzsprout.com/1790481.rss",
+//   },
+// ];
+
+// app.get("/rss", async (req, res) => {
+//   try {
+//     const parser = new Parser();
+//     const promises = rssFeeds.map(async (feed) => {
+//       const feedData = await parser.parseURL(feed.url);
+//       const itemsHtml = feedData.items.map((item) => {
+//         return `<h3>${item.title}</h3><p>${item.contentSnippet}</p><a href="${item.link}">Read More</a>`;
+//       });
+//       const html = `<html><head><title>${feed.name}</title></head><body>${itemsHtml.join("")}</body></html>`;
+//       return html;
+//     });
+//     const html = (await Promise.all(promises)).join("");
+//     res.send(html);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send("Internal Server Error");
+//   }
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server running on http://localhost:${port}`);
+// });
