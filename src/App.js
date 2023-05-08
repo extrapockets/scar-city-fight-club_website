d89dcd245e9fd3b86d327d94ba965efd4a5be411
extrapockets <extrapockets@proton.me>
2023-05-07 19:32:28 -0500

    initial commit to new branch

--- src/App.js
+++ src/App.js
@@ -6,12 +6,9 @@ import NewsFeed from "./components/NewsFeed";
 const newsFeeds = [
   {
     name: "News:",
+    // change to URL for Render web service deployment
     url: "http://localhost:3001/rss",
   },
-  // {
-  //   name: "Muay Thai News",
-  //   url: "http://localhost:8080/https://hnrss.org/newest?q=FOSS+OR+privacy"
-  // }
 ];
 
 const App = () => {
@@ -28,38 +25,3 @@ const App = () => {
 };
 
 export default App;
-
-
-
-
-
-// import React from "react";
-// import NewsFeed from "./components/NewsFeed";
-
-// const newsFeeds = [
-//   {
-//     name: "Bitcoin News",
-//     url: "https://cors-anywhere.herokuapp.com/https://nobsbitcoin.com/rss/"
-//   },
-//   {
-//     name: "Muay Thai News",
-//     url: "https://cors-anywhere.herokuapp.com/https://hnrss.org/newest?q=FOSS+OR+privacy"
-//   }
-//   ];
-
-// const App = () => {
-//   return (
-//     <div className="app">
-//       <h1>News Feed</h1>
-//       <div className="app-wrapper">
-//         {newsFeeds.map((feed) => (
-//           <NewsFeed name={feed.name} url={feed.url} key={feed.name} />
-        
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default App;
-
