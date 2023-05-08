2138d6fa44f605c5ea8cd5e110a0c2c4cb8e9496
extrapockets <extrapockets@proton.me>
2023-05-07 20:47:46 -0500

    updated csp and app.js url

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 
+// Set the Content Security Policy
+// img-src 'self' https://scar-city-feed.onrender.com/favicon.ico;
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self'; img-src 'self'; font-src 'self'; style-src 'self'; script-src 'self'; object-src 'none'; frame-ancestors 'none'; form-action 'self'; base-uri 'self'; frame-src 'none'; manifest-src 'self'; connect-src 'self'; media-src 'self'; child-src 'self'; worker-src 'self'; script-src-elem 'self' 'unsafe-inline';"
+  );
+  next();
+});
+
 app.get("/rss", async (req, res) => {
   try {
     const html = await getSortedHtml(); // Use the getSortedHtml() function
