1ecfb8dd9743e1e13940fffeb8ede4cb66ddaa9e
extrapockets <extrapockets@proton.me>
2023-05-09 22:59:55 -0500

    favicon & cors

--- server.js
+++ server.js
@@ -6,13 +6,12 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // To allow cross-origin requests from any domain:
-// app.use(function(req, res, next) {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
 
-const allowedOrigins = [
-  '*'
+// const allowedOrigins = [
   // 'http://localhost:3000',
   // 'https://scar-city-branch-csp-change1.onrender.com',
   // 'https://scar-city-branch-csp-change1.onrender.com/rss',
@@ -21,21 +20,21 @@ const allowedOrigins = [
   // 'https://feeds.buzzsprout.com',
   // 'https://scar-city-fight-club-website-lxqidsuik-extrapockets.vercel.app/',
   // 'https://scar-city-fight-club-website-ennuvopsd-extrapockets.vercel.app/'
-];
+// ];
 
 // Set up the CORS options
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-};
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     if (allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   }
+// };
 
 // Use the CORS middleware with the options
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 app.get('/rss', async (req, res) => {
   try {
