0eac15e0260d5db2427efc2f05cb3b97f3c21b57
extrapockets <extrapockets_1@pop-os.localdomain>
2023-05-15 19:30:38 -0500

    changed date format to work with 'moment'

--- rss.js
+++ rss.js
@@ -17,37 +17,36 @@ const rssFeeds = [
 ];
 
 async function getSortedHtml() {
-    try {
-      const parser = new Parser();
-      const promises = rssFeeds.map(async (feed) => {
-        const feedData = await parser.parseURL(feed.url);
-        const items = feedData.items.slice(0, 5); // limit to 5 items
-        const itemsWithDate = items.map((item) => {
-          return {
-            ...item,
-            date: moment.utc(item.pubDate).toDate(),
-          };
-        });
-        return itemsWithDate;
+  try {
+    const parser = new Parser();
+    const promises = rssFeeds.map(async (feed) => {
+      const feedData = await parser.parseURL(feed.url);
+      const items = feedData.items.slice(0, 5); // limit to 5 items
+      const itemsWithDate = items.map((item) => {
+        return {
+          ...item,
+          date: moment(item.isoDate || item.pubDate).toDate(),
+        };
       });
-      const itemsArrays = await Promise.all(promises);
-      const sortedItems = itemsArrays
-        .flat()
-        .sort((a, b) => moment.utc(b.date).diff(moment.utc(a.date)));
-      const itemsHtml = sortedItems.map((item) => {
-        return `<h3>${item.title}</h3><p>${item.contentSnippet}</p><p>${moment
-          .utc(item.date)
-          .format("MMMM Do YYYY, h:mm:ss a")}</p><a href="${item.link}">Read More</a>`;
-      });
-      const html = `<html><head><title>RSS Feed</title></head><body>${itemsHtml.join(
-        ""
-      )}</body></html>`;
-      return html;
-    } catch (error) {
-      console.error(error);
-      throw new Error("Error getting sorted RSS feed HTML");
-    }
+      return itemsWithDate;
+    });
+    const itemsArrays = await Promise.all(promises);
+    const sortedItems = itemsArrays
+      .flat()
+      .sort((a, b) => moment.utc(b.date).diff(moment.utc(a.date)));
+    const itemsHtml = sortedItems.map((item) => {
+      return `<h3>${item.title}</h3><p>${item.contentSnippet}</p><p>${moment
+        .utc(item.date)
+        .format("MMMM Do YYYY, h:mm:ss a")}</p><a href="${item.link}">Read More</a>`;
+    });
+    const html = `<html><head><title>RSS Feed</title></head><body>${itemsHtml.join(
+      ""
+    )}</body></html>`;
+    return html;
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error getting sorted RSS feed HTML");
   }
-  
-module.exports = { getSortedHtml };
+}
 
+module.exports = { getSortedHtml };
